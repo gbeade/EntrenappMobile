0245eee87b43b93cc283d972d853117748be989f
shadad00 <shadad@itba.edu.ar>
2021-11-11 21:57:59 -0300

    Se agrego cambios de fragmentos en el BottomNavigation

--- app/src/main/java/com/example/entrenapp/BodyActivity.java
+++ app/src/main/java/com/example/entrenapp/BodyActivity.java
@@ -2,20 +2,31 @@ package com.example.entrenapp;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.View;
 
-import com.example.entrenapp.databinding.ActivityBodyBinding;
-import com.example.entrenapp.databinding.ActivityYourRoutinesBinding;
-import com.example.entrenapp.databinding.ToolbarMainBinding;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BodyActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_body);
+//        ActivityBodyBinding binding = ActivityBodyBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+
+         bottomNavigationView = findViewById(R.id.bottom_navigation);
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView,navHostFragment.getNavController());
+
+
 //        ActivityBodyBinding binding = ActivityBodyBinding.inflate(getLayoutInflater());
 //        View root = binding.getRoot();
 //
