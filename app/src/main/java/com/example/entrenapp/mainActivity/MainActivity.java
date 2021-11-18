8fb83bbdf1e322c1cc993e413903bd8a17006b2f
mdedeu <mdedeu@itba.edu.ar>
2021-11-18 10:50:29 -0300

    Loading

--- app/src/main/java/com/example/entrenapp/mainActivity/MainActivity.java
+++ app/src/main/java/com/example/entrenapp/mainActivity/MainActivity.java
@@ -83,12 +83,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 return;
             } else if(r.getStatus() == Status.ERROR){
-                Snackbar snackbar = Snackbar.make(binding.btnLogin, R.string.errorLogin, BaseTransientBottomBar.LENGTH_SHORT);
+                Snackbar snackbar = Snackbar.make(binding.btnLogin, R.string.errorLogin, BaseTransientBottomBar.LENGTH_LONG);
                 View snackBarView = snackbar.getView();
                 snackBarView.setBackgroundColor(Color.RED);
                 snackbar.show();
                 return;
-            }else if (r.getStatus() != Status.LOADING){
+            }else if (r.getStatus() == Status.LOADING){
+                Snackbar snackbar = Snackbar.make(binding.btnLogin, R.string.loading, BaseTransientBottomBar.LENGTH_SHORT);
+                snackbar.show();
+                return;
+            }else{
                 Snackbar snackbar = Snackbar.make(binding.btnLogin, R.string.unknown, BaseTransientBottomBar.LENGTH_SHORT);
                 View snackBarView = snackbar.getView();
                 snackbar.show();
